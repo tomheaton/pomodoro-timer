e9e7cb768e785a512a79a935696a1320f86c07c0
Thomas Heaton <thomas.m.heaton@gmail.com>
2022-09-08 20:21:41 +0100

    initial commit :bulb:

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './images/logo.svg';
+import './styles/App.css';
+
+const App: React.FC = () => {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <h1>
+                    Pomodoro Timer
+                </h1>
+                <p>
+
+                </p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
